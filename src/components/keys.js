b7cc2358f40e48a29198a93ebb2ee5064874325c
Dev Singh <devrajput18vk@gmail.com>
2025-09-24 09:19:17 +0000

    fg

--- src/components/keys.js
+++ src/components/keys.js
@@ -1,2 +1,1 @@
-export const link = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDQcjdZdvv7a63nYgW8MEDBmep1RYM2iag'
-// export const key = ''
+export const link = import.meta.env.VITE_API_KEY
